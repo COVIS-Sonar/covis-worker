01e2e097a1d56eb55903b8e9da84b68c549ebfac
Aaron Marburg <amarburg@apl.washington.edu>
2018-02-02 16:39:35 -0800

    Upgraded the query for files on NAS.

--- covis_db/qc.py
+++ covis_db/qc.py
@@ -19,8 +19,17 @@ onc_count = runs.find({'site': 'ONC'}).count()
 print("% 6d are located at ONC" % onc_count )
 
 ## Find runs without a DMAS entry
-cursor = runs.find({'site': 'ONC', 'raw.host': 'DMAS'})
-dmas_count = cursor.count()
+dmas_count = runs.find({'site': 'ONC', 'raw.host': 'DMAS'}).count()
 print("% 6d are located at ONC, and have entries at DMAS" % dmas_count )
+
+
+## I'm sure this is a terrible, terrible thing
+nas_count = runs.find({'site': 'ONC', "$or": [{'raw.host': 'COVIS-NAS1'},
+                                              {'raw.host': 'COVIS-NAS2'},
+                                              {'raw.host': 'COVIS-NAS3'},
+                                              {'raw.host': 'COVIS-NAS4'},
+                                              {'raw.host': 'COVIS-NAS5'},
+                                              {'raw.host': 'COVIS-NAS6'}]}).count()
+print("% 6d are located on at least one NAS" % nas_count)
 
 print("% 6d ONC files exist somewhere but not at DMAS" % (onc_count - dmas_count))
