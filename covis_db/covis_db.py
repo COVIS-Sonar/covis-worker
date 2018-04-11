d68f01060ba86bc9666b819f5f875f89574fca80
Aaron Marburg <amarburg@apl.washington.edu>
2018-04-10 20:31:31 -0700

    Cursory basename test.

--- covis_db/covis_db.py
+++ covis_db/covis_db.py
@@ -2,9 +2,6 @@
 from pymongo import MongoClient
 
 
-def foobar():
-    print("Hello!")
-
 class CovisDB:
 
     def __init__( self, db_client = None ):
@@ -17,9 +14,14 @@ class CovisDB:
         self.db = self.client.covis
         self.runs = self.db.runs
 
-    def find( self, basename ):
+    def find( self, basename=None ):
         if basename:
-            self.find_by_basename(basename)
+            return self.find_by_basename(basename)
 
-    def find_by_basename( self, basename ):
-        return self.runs.find( {"basename": basename})
+    def find_by_basename(self, basename):
+        print("Searching for basename: %s" % basename)
+
+        ## Expect small returns, so unwrap
+        cursor = self.runs.find( {'basename': basename})
+
+        return [p for p in cursor]
--- test/test_covis_db.py
+++ test/test_covis_db.py
@@ -1,8 +1,19 @@
 
 from covis_db import covis_db
 
-def test_init_db():
+good_basenames = ["APLUWCOVISMBSONAR001_20100930T153416.845Z-DIFFUSE"]
 
-    covis_db.foobar()
+bad_basenames = ["APLUWCOVISMBSONAR001_20141215T030021.657Z-IMAGING"]
+
+def test_find_basenames():
 
     db = covis_db.CovisDB()
+
+    for bn in good_basenames:
+        results = db.find(basename=bn)
+        assert results
+        assert len(results) == 1
+
+    for bn in bad_basenames:
+        results = db.find(basename=bn)
+        assert results == []
