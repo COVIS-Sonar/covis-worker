563e3f35f3921e16a7cdc80b9379710aeafe7740
Aaron Marburg <amarburg@notetofutureself.org>
2018-06-20 12:07:30 -0700

    Calculating contents of tar archive.

--- covis_worker/rezip.py
+++ covis_worker/rezip.py
@@ -8,6 +8,9 @@ import os
 from subprocess import Popen,PIPE
 import gzip
 import shutil
+import tarfile
+
+import hashlib as hash
 
 from covis_db import hosts,db
 
@@ -53,16 +56,24 @@ def rezip(basename, dest_host, dest_fmt='7z', src_host=[], tempdir=None):
     do_update_contents = True
 
     if do_update_contents:
-        decompressed_path = pathlib.Path("/tmp", basename)
-        with open(decompressed_path,'wb') as data:
-            shutil.copyfileobj(data, process.stdin)
+        decompressed_path = "/tmp"
 
+        with tarfile.open(fileobj=r,mode="r:*") as tf:
+            tf.extractall(path=decompressed_path)
+            mem = tf.members
+
+            contents = [tarInfoToContentsEntry(ti, decompressed_path) for ti in mem]
+
+            print(contents)
+
+        return True
     else:
         # If not updating contents, this can be done as a stream operation
         command = ["7z", "a", "-si", "-y", outfile]
         with Popen(command, stdin=PIPE) as process:
             with gzip.GzipFile(fileobj=r) as data:
                 shutil.copyfileobj(data, process.stdin)
+
 
     # Check the results
     command = ["7z", "t", outfile]
@@ -78,3 +89,27 @@ def rezip(basename, dest_host, dest_fmt='7z', src_host=[], tempdir=None):
     logging.info("Updating DB")
     raw = {'host': dest_host, 'path': raw.filename}
     print(raw)
+
+
+
+def tarInfoToContentsEntry(ti, workdir):
+    ## Calculated
+    sha = shasum(pathlib.Path(workdir, ti.name))
+
+    return {'name': ti.name,
+            'size': ti.size,
+            'sha1': sha}
+
+
+def shasum(path):
+    # Specify how many bytes of the file you want to open at a time
+    BLOCKSIZE = 65536
+
+    sha = hash.sha1()
+    with open(path, 'rb') as infile:
+        file_buffer = infile.read(BLOCKSIZE)
+        while len(file_buffer) > 0:
+            sha.update(file_buffer)
+            file_buffer = infile.read(BLOCKSIZE)
+
+    return sha.hexdigest()
