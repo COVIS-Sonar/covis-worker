2d184dd4ae6e7c47ab9d04988e1e63078d2df0d0
Aaron Marburg <amarburg@uw.edu>
2019-04-03 14:43:50 -0700

    Mild cleanup on run_metadata_report.py

--- apps/run_metadata_report.py
+++ apps/run_metadata_report.py
@@ -15,22 +15,6 @@ from os import path
 
 from covis_db import db,hosts,remote,misc
 
-
-#import contextlib
-
-# @contextlib.contextmanager
-# def smart_open(filename=None):
-#     if filename and filename != '-':
-#         fh = open(filename, 'w', newline='')
-#     else:
-#         fh = sys.stdout
-#
-#     try:
-#         yield fh
-#     finally:
-#         if fh is not sys.stdout:
-#             fh.close()
-
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--dbhost', default=config('MONGODB_URL', default="mongodb://localhost/"),
@@ -45,13 +29,11 @@ parser.add_argument('--output', default=None)
 ## Limit of 0 means "no limit" to the MongoDB limit() function
 parser.add_argument('--count', type=int, default=0, help='')
 
-parser.add_argument("--sites", nargs='*', default=['onc','ooi'])
+parser.add_argument("--sites", nargs='*', default=['Ashes','Endeavour'])
 
 
 args = parser.parse_args()
 logging.basicConfig( level=args.log.upper() )
-
-##
 sites = [s.lower() for s in args.sites]
 
 
@@ -62,21 +44,22 @@ else:
 
 writer = csv.writer(outfile)
 
-db_client = MongoClient(args.dbhost)
-client = db.CovisDB(db_client)
+
+client = db.CovisDB(MongoClient(args.dbhost))
 
 outfile.write("# %s\n" % ','.join(['basename','datetime',\
                 'site','mode','raw_size','gz_size','7z_size']) )
 
-for run in client.runs.find().limit( args.count ):
+for run in client.runs.find({}, limit=args.count ):
 
     run = db.CovisRun(run, collection=client.runs)
 
-    if ('ooi' in sites and run.datetime > datetime.datetime(2018,7,1)) or \
-       ('onc' in sites and run.datetime < datetime.datetime(2018,1,1)):
+    if ('ashes' in sites and run.datetime > datetime.datetime(2018,7,1)) or \
+       ('endeavour' in sites and run.datetime < datetime.datetime(2018,1,1)):
 
         logging.debug("Checking basename %s" % run.basename)
 
+        ## Set these to None so the CSV field is empty if these values can't be determined
         raw_size = None
         nas_size = None
         gz_size = None
@@ -84,6 +67,9 @@ for run in client.runs.find().limit( args.count ):
         if 'contents' in run.json:
             for file in run.json['contents']:
                 if 'size' in file:
+                    if not raw_size:
+                        raw_size = 0
+
                     raw_size = raw_size + file['size']
 
         for raw in run.raw:
@@ -92,7 +78,7 @@ for run in client.runs.find().limit( args.count ):
             elif hosts.is_wasabi(raw.host):
                 gz_size = raw.filesize
 
-        row = [run.basename, run.datetime,  \
+        row = [run.basename, run.datetime.isoformat(),  \
                 run.site, run.mode,
                 raw_size, gz_size, nas_size ]
 
