372657ae8197e29665d5139c5e89d768b0e89ae8
Aaron Marburg <aaron@uw.edu>
2020-11-14 23:04:14 -0800

    Minor change to sync script.

--- apps/sync_postprocessed_to_ooi.sh
+++ apps/sync_postprocessed_to_ooi.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+#
+# sftp -i ~/.ssh/covis.privkey covis@pi.ooirsn.uw.edu
 
-rclone sync  --progress --sftp-host pi.ooirsn.uw.edu --sftp-user covis --sftp-key-file $HOME/.ssh/covis.privkey  /auto/nas/covis/data/postprocessed/Version3.2_automatic/  :sftp:processed/Version3.2_automatic/
+rclone sync  --verbose --sftp-host pi.ooirsn.uw.edu --sftp-user covis --sftp-key-file $HOME/.ssh/covis.privkey  /auto/nas/covis/data/postprocessed/Version3.2_automatic/  :sftp:processed/Automatic_Postprocessing_Version3.2/
